4afc500c04fe8d354248eeda9cc3e654be3f91b4
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-11-03 21:39:50 +0000

    work in progress

--- includes/hero.php
+++ includes/hero.php
@@ -14,12 +14,12 @@
 <section class="section">
   <div class="container">
     <div class="columns  has-background-light">
-      <div class="column is-9">
-        <img src="/img/slides/festo-jose-joao.jpg" alt="">
-      </div>
       <div class="column is-3">
         <h2 class="title is-size-3">Welcome To HPE Industry</h2>
         <p class="subtitle is-size-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolorum expedita, exercitationem dolor voluptatibus omnis ad ex sint nulla nihil labore obcaecati aperiam, aliquam illum. Ullam omnis consequatur nesciunt doloribus?</p>
+      </div>
+      <div class="column is-9">
+        <img src="/img/slides/blue-festo-jose-joao.jpg" alt="">
       </div>
     </div>
     <!-- <img src="/img/slides/festo-jose-joao.jpg" alt=""> -->
