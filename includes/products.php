7898376939571e722471b35ba3286065edec8569
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-11-12 05:01:11 +0000

    work in progress

--- includes/products.php
+++ includes/products.php
@@ -1,6 +1,5 @@
         <h2 class="title is-size-3">Products</h2>
-        <p class="subtitle is-size-5">H.P.E. INDUSTRY LTD. stocks a wide range of FESTO, AIGNEP, AUTONICS, COMEX, FINDER, INFINITY, JORC, M&M ROTORK, MEI, OMI, and SHINY products from valves and cylinders to sensors, pumps and motors in Ghana.</p>
-        <hr>
+        <p class="subtitle pull-quote is-size-5">H.P.E. INDUSTRY LTD. stocks a wide range of FESTO, AIGNEP, AUTONICS, COMEX, FINDER, INFINITY, JORC, M&M ROTORK, MEI, OMI, and SHINY products from valves and cylinders to sensors, pumps and motors in Ghana.</p>
         <div class="columns">
             <div class="column"><a href="/products/festo"><img src="img/product_buttons/festo.jpg" alt=""></a></div>
             <div class="column"><a href=""><img src="img/product_buttons/aignep.png" alt=""></a></div>
--- includes/services.php
+++ includes/services.php
@@ -1,36 +1,23 @@
-<h2 class="title is-size-3">Services</h2>
+<h2 class="title has-padding-bottom is-size-3">Services</h2>
 
 <div style="padding:0.5em" class="has-background-light">
 
-<h3 class="title is-size-5">Sales and Services</h3>
+<p>HPE INDUSTRY LTD. provides the best industrial automation solutions and technical support through our highly experienced sales team. </p>
 
-<p>HPE INDUSTRY LTD. provides the best industrial automation solutions and technical support through our highly experienced sales team. We are available to help you at any time in order to achieve your total satisfaction.</p>
+<h3 class="has-padding-bottom is-size-5 has-padding-top">Solution Sales</h3>
+<p> With a wide variety of products, we help customers raise productivity by providing alround solutions which are customized to meet requirements, budget and environments.</p>
 
-<h3 class="title is-size-5">Solution Sales</h3>
-<p> With a wide variety of products, we help customers raise productivity by providing alround solutions which are customized to meet requirements, budget and environments. We always strives to meet customer needs by constantly developing new products. We showcase our new products and listen to our customers.</p>
-
-<h3 class="title is-size-5">Compressed Air Installation</h3>
+<h3 class="has-padding-bottom is-size-5 has-padding-top">Compressed Air Installation</h3>
 
 <p>
-    Infinity piping is a high performance aluminum piping system that is easy to install and simple to extend or modify. Available in air, nitrogen, and vacuum, and in a wide range of diameters, the non-corrosive, extruded aluminum of Infinity pipe provides structural strength and delivers contaminate and leak free fluids.
-</p>
+    Infinity piping is a high performance aluminum piping system that is easy to install and simple to extend or modify. </p>
+<h3 class="has-padding-bottom has-padding-top is-size-5">Maintenance & Field Work</h3>
 
 <p>
-    The smooth bore of Infinity compressed air piping prevents high pressure losses and allows for higher flow rates and performance than that of other systems. No fluid leakage ensures minimum running costs.
+    We provide field support and services for large scale projects within a wide range of industries. With a vast range of products including aluminum piping. 
 </p>
 
-<p>
-    Infinity piping provides customers with top of the range high quality systems that are energy efficient and require minimum running costs. Customers are guaranteed technical expertise, superior service, and unlimited access to the highest quality equipment at competitive industry prices.
-</p>
-
-<h3 class="title is-size-5">Maintenance & Field Work</h3>
-
-<p>
-    We provide field support and services for large scale projects within a wide range of industries. With a vast range of products including aluminum piping, filtration, fittings, valves, cylinders and tubing, we have the versatility to ensure you have the most efficient compressed air solution.
-    Our aim is to bring our customers technical expertise, superior workmanship across all sized projects and unlimited access to the most advanced pneumatic equipment on the market. Our highly experienced design engineers are available to provide professional advice on any pneumatic projects. 
-</p>
-
-<h3 class="title is-size-5">industrial automation</h3>
+<h3 class="has-padding-bottom is-size-5 has-padding-top">Industrial Automation</h3>
 <p>We are dedicated to providing ongoing support and industry solutions in electro-pneumatic, Industrial Automation with a highly cost Effective Solution products of Sensors.</p>
 
 </div>
