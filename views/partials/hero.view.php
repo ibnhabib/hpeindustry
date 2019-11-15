8e3669b8623d64b15c4bb882c026d4a01880ad60
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-11-15 13:56:49 +0000

    added contact us button on hero

--- views/partials/hero.view.php
+++ views/partials/hero.view.php
@@ -16,7 +16,8 @@
     <div class="columns  has-background-light">
       <div class="column is-3">
         <h1 class="title is-size-3">Welcome To HPE Industry</h1>
-        <p class="is-size-5">Established in April 2005, H.P.E. INDUSTRY LTD. is a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of equipment and services</p>
+        <p class="is-size-5">Established in April 2005, we are a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of equipment and services from top international brands</p>
+        <button class="button is-primary is-medium" style="margin-top:1.5em">Get In Touch</button>
       </div>
       <div class="column is-paddingless is-9">
         <img src="/img/slides/blue-festo-jose-joao.jpg" alt="">
