affdbf92982ee649473990e96cccf0d0fd44686c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-11-23 19:44:19 +0000

    work in progress

--- views/partials/homepage/products.view.php
+++ views/partials/homepage/products.view.php
@@ -15,7 +15,7 @@ $products=[
 
 ?>
 <h2 class="title is-size-3">Products</h2>
-<p class="subtitle pull-quote is-size-5">H.P.E. Industry LTD. stocks a wide range of products from valves and cylinders to sensors, pumps and motors in Ghana, all from top international brands listed below.</p>
+<p class="pull-quote is-size-5">H.P.E. Industry LTD. stocks a wide range of products from valves and cylinders to sensors, pumps and motors in Ghana, all from top international brands listed below.</p>
 <div class="hpe_grid" style="flex-wrap: wrap; display: flex; justify-content: space-between;">
 <?php
     foreach ($products as $key => $product) { ?>
