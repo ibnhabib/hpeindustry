45c5f24069aee1227d643da4dd995fac5a9ccb95
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-11-23 22:29:55 +0000

    improved hero image

--- views/partials/homepage/hero.view.php
+++ views/partials/homepage/hero.view.php
@@ -1,26 +1,13 @@
-<!-- <section class="hero has-background">
-  <img src="/img/slides/festo-jose-joao.jpg" alt="" class="hero-background is-transparent">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-        Welcome to HPE Industry LTD
-      </h1>
-      <h2 class="subtitle">
-        Your Reliable Partner in Supplying and Servicing parts
-      </h2>
-    </div>
-  </div>
-</section> -->
 <section class="section">
   <div class="container">
-    <div class="columns  has-background-light">
-      <div class="column is-3">
+    <div class="columns has-background-light">
+      <div class="hpe-blurb column is-3">
         <h1 class="title is-size-3">Welcome To HPE Industry</h1>
         <p class="is-size-5">Established in April 2005, we are a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of equipment and services from top international brands</p>
         <button class="button is-primary is-medium" style="margin-top:1.5em">Get In Touch</button>
       </div>
-      <div class="column is-paddingless is-9">
-        <img src="/img/slides/blue-festo-jose-joao.jpg" alt="">
+      <div class="hero-background column is-paddingless is-9">
+        <!-- <img src="/img/slides/blue-festo-jose-joao.jpg" alt=""> -->
       </div>
     </div>
     <!-- <img src="/img/slides/festo-jose-joao.jpg" alt=""> -->
