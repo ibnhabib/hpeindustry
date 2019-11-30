3bb3893fdc65e309424233026667b719411ab8c7
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-11-30 16:18:51 +0000

    added raw elements to about us page

--- views/about.view.php
+++ views/about.view.php
@@ -1,1 +1,57 @@
 <h1>about page</h1>
+
+
+Established in April 2005, H.P.E. INDUSTRY LTD. is a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of hydraulics, pneumatics and electronic equipment, as well as air-filtration, drives, instrumentation, valves etc.
+
+We are authorized distributors and service providers for FESTO, AIGNEP, AUTONICS, COMEX, FINDER, INFINITY, JORC, M&M ROTORK, MEI, OMI, and SHINY products in Ghana.
+
+We stock and provide only superior products from trusted names!
+
+Our technicians and factory trained engineers (who work with us since the very beginning of H.P.E. INDUSTRY LTD.), are experienced experts (certified engineers), in their respective mechanical, electromechanical, and industrial engineering fields.
+
+Because of this, H.P.E. INDUSTRY LTD. has earned a reputation of being among the best in the industry, a large part of which is due to the fact that we have a genuine interest in our customer’s success.
+
+Our experienced and service-oriented team in our head office in Accra, has the complete understanding of all the components and specific requirements.
+
+With a commitment to swift, reliable and responsible service, we can deliver all these products, allowing your production to stay operational at all times.
+
+Whether you work in mining, agriculture, food processing or other industry.
+
+We are giving you the confidence that only best-in-class products are being fitted for your application or installation.
+
+To inquire about our products and services, or to find out more about how we can assist you, do not hesitate to contact us today.
+
+The H.P.E. team is available for you!
+
+
+<h2>
+    Quality Assurance
+</h2>
+<img src="/img/festo_technicians.jpg" alt="">
+We stand behind every product that leaves our doors.
+
+Each component is carefully designed, tested, re-tested, checked and rechecked until we are 100% satisfied it meets stringent performance standards and exact specifications.
+
+Our up-to-date technology and advanced training assures you get the very best product available.
+
+<h2>
+    Customer Service
+</h2>
+
+At H.P.E. INDUSTRY LTD. we are always ready and able to answer or assist you with any questions or problems you may have.
+
+No customer is too small. No question goes unanswered.
+
+We pride ourselves on responding to the needs of our customers – before and long after the point of sale.
+
+We are able to make any modifications to existing products to fit your specific applications.
+
+We expect nothing less than complete customer satisfaction.
+<h2>
+    Core Values
+</h2>
+
+    Safety, integrity and responsibility in everything we do
+    Highly experienced and competent Service Team
+    Fast & friendly support, attention to every detail
+
