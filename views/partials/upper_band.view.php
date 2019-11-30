bb5fa33fc415f19e3ea9caec196e8ac07f2650e0
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-11-30 16:04:57 +0000

    Upper band. Added Icons and layout

--- views/partials/upper_band.view.php
+++ views/partials/upper_band.view.php
@@ -1,5 +1,35 @@
 <section class="section has-background-primary has-little-padding">
-    <div class="container has-text-white">
-    +233 (0) 302 235 187    <span class="is-pulled-right">sales@hpeindustry.com</span>    
+    <div class="container hpe_band">
+        <nav class="level is-marginless">
+            <div class="level-left">
+                <div class="level-item">
+                    <div class="level-icon">
+                        <?php include_once($project_root_folder.'/img/icons/telephone.svg') ?>
+                    </div>
+                    <div>
+                        +233 (0) 302 235 187
+                    </div> 
+                </div>
+                <div class="level-item">
+                    <div class="level-icon">
+                        <?php include_once($project_root_folder.'/img/icons/email.svg') ?>
+                    </div>
+                    <div>
+                        <a href="mailto:sales@hpeindustry.com">sales@hpeindustry.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="level-right">
+                <div class="level-item">
+                    <div class="level-icon">
+                        <?php include_once($project_root_folder.'/img/icons/facebook.svg') ?>
+                    </div>
+                    <div>
+                       <a href="https://web.facebook.com/hpeindustry">Facebook Page</a> 
+                    </div>
+                </div>
+   
+            </div>
+        </nav>
     </div>
 </section>
