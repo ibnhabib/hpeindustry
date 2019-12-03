8db9da814c5e7818d65a88fa55766730b06935e9
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-12-03 21:30:04 +0000

    working on about page

--- views/about.view.php
+++ views/about.view.php
@@ -1,57 +1,108 @@
-<h1>about page</h1>
+<?php
+$page_title = 'About HPE Industry';
+$css = '/css/app.css';
+$script = 'js/app.js';
+?>
+
+<!-- Head, Meta Info -->
+<?php include_once($project_root_folder . '/views/partials/head.view.php'); ?>
+
+<!-- Header Band for contact -->
+<?php include_once($project_root_folder . '/views/partials/upper_band.view.php'); ?>
+
+<!-- Navigation Bar -->
+<?php include_once($project_root_folder . '/views/partials/navigation.view.php'); ?>
 
 
-Established in April 2005, H.P.E. INDUSTRY LTD. is a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of hydraulics, pneumatics and electronic equipment, as well as air-filtration, drives, instrumentation, valves etc.
+<section class="section">
+    <div class="container">
 
-We are authorized distributors and service providers for FESTO, AIGNEP, AUTONICS, COMEX, FINDER, INFINITY, JORC, M&M ROTORK, MEI, OMI, and SHINY products in Ghana.
+        <div class="columns">
+            <div class="column is-7">
+                <h3 class="title is-3">
+                    About Us
+                </h3>
+                <p>Established in April 2005, H.P.E. INDUSTRY LTD. is a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of hydraulics, pneumatics and electronic equipment, as well as air-filtration, drives, instrumentation, valves etc. We are authorized distributors and service providers for FESTO, AIGNEP, AUTONICS, COMEX, FINDER, INFINITY, JORC, M&M ROTORK, MEI, OMI, and SHINY products in Ghana. We stock and provide only superior products from trusted names!</p>
 
-We stock and provide only superior products from trusted names!
+                <p>Our technicians and factory trained engineers (who work with us since the very beginning of H.P.E. INDUSTRY LTD.), are experienced experts (certified engineers), in their respective mechanical, electromechanical, and industrial engineering fields.</p>
 
-Our technicians and factory trained engineers (who work with us since the very beginning of H.P.E. INDUSTRY LTD.), are experienced experts (certified engineers), in their respective mechanical, electromechanical, and industrial engineering fields.
+                <p>
+                    Because of this, H.P.E. INDUSTRY LTD. has earned a reputation of being among the best in the industry, a large part of which is due to the fact that we have a genuine interest in our customer’s success.
+                </p>
+            </div>
+            <div class="column is-5"><img src="/img/festo_technicians.jpg" alt=""></div>
+        </div>
+        <hr>
+        <div class="columns">
+            <div class="column">
+                <img src="/img/pipes-1.jpg">
+                <p>
+                    Our experienced and service-oriented team in our head office in Accra, has the complete understanding of all the components and specific requirements.
+                </p>
 
-Because of this, H.P.E. INDUSTRY LTD. has earned a reputation of being among the best in the industry, a large part of which is due to the fact that we have a genuine interest in our customer’s success.
+                <p>
+                    With a commitment to swift, reliable and responsible service, we can deliver all these products, allowing your production to stay operational at all times.
+                </p>
+            </div>
+            <div class="column">
+                <p>
+                    Whether you work in mining, agriculture, food processing or other industry.
+                </p>
 
-Our experienced and service-oriented team in our head office in Accra, has the complete understanding of all the components and specific requirements.
+                <p>
+                    We are giving you the confidence that only best-in-class products are being fitted for your application or installation.
+                </p>
+                <p>
+                    To inquire about our products and services, or to find out more about how we can assist you, do not hesitate to contact us today. The H.P.E. team is available for you!
+                </p>
 
-With a commitment to swift, reliable and responsible service, we can deliver all these products, allowing your production to stay operational at all times.
+                <h3 class="title">
+                    Quality Assurance
+                </h3>
 
-Whether you work in mining, agriculture, food processing or other industry.
+                <p>
+                    We stand behind every product that leaves our doors.
+                </p>
 
-We are giving you the confidence that only best-in-class products are being fitted for your application or installation.
+                <p>
+                    Each component is carefully designed, tested, re-tested, checked and rechecked until we are 100% satisfied it meets stringent performance standards and exact specifications.
+                </p>
 
-To inquire about our products and services, or to find out more about how we can assist you, do not hesitate to contact us today.
+                <p>
+                    Our up-to-date technology and advanced training assures you get the very best product available.
+                </p>
+                <h3 class="title">
+                    Core Values
+                </h3>
 
-The H.P.E. team is available for you!
+                <p>
+                    Safety, integrity and responsibility in everything we do Highly experienced and competent Service Team. Fast & friendly support, attention to every detail
+                </p>
+            </div>
+            <div class="column">
+
+                <h3 class="title">
+                    Customer Service
+                </h3>
+
+                <p>
+                    At H.P.E. INDUSTRY LTD. we are always ready and able to answer or assist you with any questions or problems you may have.No customer is too small. No question goes unanswered.
+                </p>
+
+                <p>
+                    We pride ourselves on responding to the needs of our customers – before and long after the point of sale.We are able to make any modifications to existing products to fit your specific applications.
+                </p>
+
+                <p>
+                    We expect nothing less than complete customer satisfaction.
+                </p>
 
 
-<h2>
-    Quality Assurance
-</h2>
-<img src="/img/festo_technicians.jpg" alt="">
-We stand behind every product that leaves our doors.
+            </div>
+        </div>
 
-Each component is carefully designed, tested, re-tested, checked and rechecked until we are 100% satisfied it meets stringent performance standards and exact specifications.
 
-Our up-to-date technology and advanced training assures you get the very best product available.
-
-<h2>
-    Customer Service
-</h2>
-
-At H.P.E. INDUSTRY LTD. we are always ready and able to answer or assist you with any questions or problems you may have.
-
-No customer is too small. No question goes unanswered.
-
-We pride ourselves on responding to the needs of our customers – before and long after the point of sale.
-
-We are able to make any modifications to existing products to fit your specific applications.
-
-We expect nothing less than complete customer satisfaction.
-<h2>
-    Core Values
-</h2>
-
-    Safety, integrity and responsibility in everything we do
-    Highly experienced and competent Service Team
-    Fast & friendly support, attention to every detail
-
+    </div>
+</section>
+<!-- Footer -->
+<?php include_once($project_root_folder . '/views/partials/footer.view.php'); ?>
