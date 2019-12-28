4b1e871d1c8cc6f7eeb38b90639b017eb59f6a51
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-12-28 17:21:10 +0000

    added more slides to home page

--- views/partials/homepage/hero.view.php
+++ views/partials/homepage/hero.view.php
@@ -6,10 +6,18 @@
         <p class="is-size-5">Established in April 2005, we are a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of equipment and services from top international brands</p>
         <button class="button is-primary is-medium" style="margin-top:1.5em">Get In Touch</button>
       </div>
-      <div class="hero-background column is-paddingless is-9">
-        <!-- <img src="/img/slides/blue-festo-jose-joao.jpg" alt=""> -->
+      <div class="hero-background column is-paddingless is-9" style="background-image: url('/img/slides/<?php echo random_slide()  ?>')"> 
       </div>
     </div>
     <!-- <img src="/img/slides/festo-jose-joao.jpg" alt=""> -->
   </div>
 </section>
+
+<?php 
+
+function random_slide(){
+  $slides= ['festo-jose-joao.jpg','festo-man-woman.jpg','festo-globe.jpg'];
+  return $slides[array_rand($slides)];;
+}
+
+?>
