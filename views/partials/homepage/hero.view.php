2626df1cc2348830b9f7ec28724a0e35aa257f07
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2020-04-26 12:37:09 +0000

    fixed hero button to link to contact us page

--- views/partials/homepage/hero.view.php
+++ views/partials/homepage/hero.view.php
@@ -4,7 +4,7 @@
       <div class="hpe-blurb column is-3">
         <h1 class="title is-size-3">Welcome To HPE Industry</h1>
         <p class="is-size-5">Established in April 2005, we are a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of equipment and services from top international brands</p>
-        <button class="button is-primary is-medium" style="margin-top:1.5em">Get In Touch</button>
+        <a href="/contact" class="button is-primary is-medium" style="margin-top:1.5em">Get In Touch</a>
       </div>
       <div class="hero-background column is-paddingless is-9" style="background-image: url('/img/slides/<?php echo random_slide()  ?>')"> 
       </div>
