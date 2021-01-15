b0d856e48e68139c670595e7f390547eea1f03b6
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2021-01-15 12:20:53 +0000

    made slideshow more responsive

--- source/assets/_partials/slideshow.blade.php
+++ source/assets/_partials/slideshow.blade.php
@@ -1,14 +1,16 @@
 
 
-<section class="container mx-auto bg-gray-100 relative flex mb-10" >
-    <div class="w-1/4 bg-gray-100">
-        <div class="p-4">
-            <h2 class="mb-4 text-2xl text-primary font-bold z-10">Welcome to HPE Industry</h2>
-            <p class="mt-4 ">Established in April 2005, we are a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of equipment and services from top international brands</p>
-            <a class= "font-bold inline-block mt-2 bg-primary px-4 py-2 hover:bg-primary-darker text-white rounded" href="#">Take Action</a> 
+<section class="container mx-auto bg-gray-100 relative lg:flex lg:flex-row mb-10" >
+    <div class="w-full lg:w-1/4 bg-gray-100">
+        <div class="p-4 lg:block">
+            <div>
+                <h2 class="mb-4 text-2xl text-primary font-bold z-10">Welcome to HPE Industry</h2>
+                <p class="mt-4 ">Established in April 2005, we are a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of equipment and services from top international brands</p>
+            </div>
+            <a class= "font-bold inline-block mt-2 bg-primary px-4 py-2 hover:bg-primary-darker text-white rounded" href="#">Get In Touch</a> 
         </div>
     </div>
-    <div class="w-3/4 bg-gray-50">
+    <div class="w-full lg:w-3/4 bg-gray-50">
         <div class="main-carousel mx-auto" data-flickity='{ "autoPlay":true, "contain": true, "bgLazyLoad": true, "imagesLoaded": true, "pageDots": false }'> 
             <div style="width:100%;	padding-bottom: 42.3%; background-size: cover; background-repeat: no-repeat;"  
                 class="carousel-cell" data-flickity-bg-lazyload="/assets/images/slides/festo-jose-joao.jpg" >
