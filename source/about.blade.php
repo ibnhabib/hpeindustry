8b500ec1cccf9c116b9e79823d37cc53928184e3
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2021-02-02 12:48:26 +0000

    refinements and more work on about us

--- source/about.blade.php
+++ source/about.blade.php
@@ -12,29 +12,49 @@ title: About HPE Industry
      <span class="hidden xl:inline-block">extra large</span>
 </div>
 
-
 @section('body')
 @include('assets._partials.upper_band')
 @include('assets._partials.navigation')
 
 <section class="section">
-   <div class="flex">
-     <div class="w-1/2">
-          <h1 class="text-2xl font-semibold text-primary">About HPE Industry LTD.</h1>
+   <div class="md:flex">
+     <div class="w-1/2 mr-4 content">
+
+          <h1 class="text-2xl font-bold text-primary">About HPE Industry LTD.</h1>
+
           <p class="mt-4 text-xl leading-relaxed text-gray-600 leading max-w-prose">
                Established in April 2005, we are a well stocked, full service hydraulic, pneumatic and electronics- company that offers a wide range of hydraulics, pneumatics and electronic equipment, as well as air-filtration, drives, instrumentation, valves etc. We are authorized distributors and service providers for FESTO, AIGNEP, AUTONICS, COMEX, FINDER, INFINITY, JORC, M&M ROTORK, MEI, OMI, and SHINY products in Ghana. We stock and provide only superior products from trusted names!
           </p>
 
-          <p class="mt-4 text-xl text-gray-600 max-w-prose">
+          <p class="text-xl text-gray-600">
           Because of this, H.P.E. INDUSTRY LTD. has earned a reputation of being among the best in the industry, a large part of which is due to the fact that we have a genuine interest in our customer’s success.
           </p>
 
      </div>
      <div class="w-1/2">
-          <img src="/assets/images/festo_technicians.jpg" alt="HPE industry Technicians">
+          <figure class="table">
+            <img src="/assets/images/festo_technicians.jpg" alt="HPE industry Technicians">
+               <div class="table-caption mt-4 text-sm text-primary" style="caption-side:bottom">
+                    Our technicians and factory trained engineers (who work with us since the very beginning of H.P.E. INDUSTRY LTD.), are experienced experts (certified engineers), in their respective mechanical, electromechanical, and industrial engineering fields.
+               </div>   
+          </figure>
+          
      </div>
 </div>  
 </section>
-
+<section class="section">
+     <div class="flex justify-between w-full pb-12">
+          <div class="w-1/3 ">
+              <img src="/assets/images/pipes-4.jpg" alt="">
+              <img src="/assets/images/pipes-1.jpg" alt="">
+          </div>
+          <div class="w-1/3 ">
+               2
+          </div>
+          <div class="w-1/3 ">
+               3
+          </div>
+     </div>
+</section>
 
 @endsection
